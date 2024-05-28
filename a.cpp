61328facfab533a5e54cd8e83192c48604151ba3
HruthikKK <149587039+HruthikKK@users.noreply.github.com>
2024-05-28 17:35:34 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -12,21 +12,24 @@ int main()
 
     sort(v.begin(),v.end());
 
-    if(v[0] == v[n-1]){
-        cout<<0<<endl;
-        return 0;
-    }
+    // if(v[0] == v[n-1]){
+    //     cout<<0<<endl;
+    //     return 0;
+    // }
 
     ll int tt = 0;
     for(int i = 0; i<n; i++) tt += v[i];
 
-    ll int toC = v[n-2];
-    ll int toD = ceil((tt+0.0)/(n-1));
+    ll int ans = 0;
+    for(int i =0; i<n; i++){
+        ll int toC = v[n-1];
+        ll int toD = ceil((tt+0.0)/(n-1));
 
-    toC = max(toC,toD);
+        toC = max(toC,toD);
 
-    ll int ans = toC*(n-1) - tt;
-        
+        ans = toC*(n-1) - tt;
+    }
+
     cout<<ans<<endl;
 	
 }
