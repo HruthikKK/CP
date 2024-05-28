b7aeb9e7ad6bf0b39752ac62824c88f7b2ff9aba
HruthikKK <149587039+HruthikKK@users.noreply.github.com>
2024-05-28 20:03:26 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -24,6 +24,35 @@ void check(ll int n){
             }
         }
     }
+    if (n > 1){
+        m[n]++;
+    }
+}
+
+bool check2(ll int n){
+    if((n%2 == 0)){
+        if(m[2]>1) return 1;
+        while (n%2 == 0)
+        {
+            n/=2;
+        }
+    }
+
+    for (int i = 3; i*i <= n; i = i+2)
+    {
+        if((n%i==0)){
+            if(m[i] > 1) return 1;
+            while (n%i == 0)
+            {
+                n = n/i;
+            }
+        }
+    }
+    if (n > 1){
+        if(m[n]>1) return true;
+    }
+
+    return false;
 }
 
 int main()
@@ -34,18 +63,24 @@ int main()
     m.clear();
 
     ll int tp;
+    vector<ll int> v(n);
     for(int i = 0; i<n; i++){
         cin>>tp;
+        v[i] = tp;
         check(tp);
     }  
 
-    for(auto x:m){
-        if(x.second == 1){
+    // for(auto x:m){
+    //     cout<<x.first<<" "<<x.second<<endl;  
+    // }
+
+    for(int i = 0; i<n; i++){
+        bool fg = check2(v[i]);
+        if(!fg){
             cout<<"No"<<endl;
             return 0;
         }
     }
-
     cout<<"Yes"<<endl;
     
 }
