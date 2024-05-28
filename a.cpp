09c3d7cedb90404bb9875c08249065ef1f435c60
HruthikKK <149587039+HruthikKK@users.noreply.github.com>
2024-05-28 19:34:36 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -2,41 +2,52 @@
 using namespace std;
 #define ll long long
 
-static bool sortbyCond(const vector<ll int>& a,const vector<ll int>& b){
-    if(a[0] == b[0]) return b[1]>a[1];
-    return a[0]>b[0];
+map<ll int, ll int> m;
+
+void check(ll int n){
+
+	if((n%2 == 0)){
+        m[2]++;
+        while (n%2 == 0)
+        {
+            n/=2;
+        }
+    }
+
+    for (int i = 3; i*i <= n; i = i+2)
+    {
+        if((n%i==0)){
+            m[i]++;
+            while (n%i == 0)
+            {
+                n = n/i;
+            }
+        }
+    }
 }
 
 int main()
 {
+
     ll int n;
     cin>>n;
+    m.clear();
 
-    vector<vector<ll int>> v(n,vector<ll int>(2));
+    ll int tp;
     for(int i = 0; i<n; i++){
-        cin>>v[i][0];
-        v[i][1] = i;
-    }
-    sort(v.begin(),v.end(),sortbyCond);
-    
-    // for(int i =0; i<n; i++){
-    //     cout<<v[i][0]<<" "<<v[i][1]<<endl; sort(v.begin(),v.end(),sortbyCond);
-    // }
+        cin>>tp;
+        check(tp);
+    }  
 
-    vector<ll int> vis(n+1,0);
-
-    ll int ans =0;
-
-    for(int i = 0; i<n; i++){
-        ll int cI = v[i][1];
-        if(!vis[cI]){
-            vis[cI] = 1;
-            vis[cI+1] = 1;
-            ans += v[i][0];
+    for(auto x:m){
+        if(x.second == 1){
+            cout<<"No"<<endl;
+            return 0;
         }
     }
 
-    cout<<ans<<endl;
+    cout<<"Yes"<<endl;
+    
 }
 
   
