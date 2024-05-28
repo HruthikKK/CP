f8e901efc7db478e2e66dc2e17f0a6739e1f5fae
HruthikKK <149587039+HruthikKK@users.noreply.github.com>
2024-05-28 18:03:40 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -2,46 +2,60 @@
 using namespace std;
 #define ll long long
 
+vector<bool> p;
+set<ll int> s;
+ll int n,k;
+
+vector<bool> sieve(ll int n){
+
+    vector<bool> v(n+1,1);
+
+    v[0] = 0;v[1] = 0;
+
+    for(ll int i = 2; i<=sqrt(n); i++){
+        if(v[i]){
+            for(ll j = i*i; j<=n; j+=i) v[j] = 0; 
+        }
+    }
+
+	p = v;
+    return v;
+}
+
+bool check(ll int n){
+
+	if(2>k && (n%2 == 0)) return 1;
+	while (n%2 == 0)
+    {
+		n/=2;
+    }
+ 
+    for (int i = 3; i*i <= n; i = i+2)
+    {
+        if(i>k && (n%i==0)) return 1;
+        while (n%i == 0)
+        {
+            n = n/i;
+        }
+    }
+ 
+    if (n > k) return 1;
+	else return 0;
+}
+
 int main()
 {
-	ll int n;
-	cin>>n;
 
-    vector<ll int> v(n);
-    for(int i =0; i<n; i++) cin>>v[i];
+	// vector<bool> p = sieve(n);
+	cin>>n>>k;
 
-    sort(v.begin(),v.end());
+	ll ans = n;
+	s.clear();
 
-    if(n == 1){
-        cout<<0<<endl;
-        return 0;
-    }
-    // if(v[0] == v[n-1]){
-    //     cout<<0<<endl;
-    //     return 0;
-    // }
+	for(int i = k+1; i<=n; i++){
+		if(check(i)) ans--;
+	}
 
-    ll int tt = 0;
-    for(int i = 0; i<n; i++) tt += v[i];
-
-    ll int ans = 0;
-    for(int i =0; i<n; i++){
-        ll int toC = v[n-1];
-        ll int toD = ceil((tt+0.0)/(n-1));
-
-        toC = max(toC,toD);
-
-        ans = toC*(n-1) - tt;
-    }
-
-    ll int toC = v[n-2];
-    ll int toD = ceil((tt+0.0)/(n-1));
-
-    toC = max(toC,toD);
-
-    ans = toC*(n-1) - tt;
-
-    cout<<ans<<endl;
-	
+	cout<<ans<<endl;
 }
   
